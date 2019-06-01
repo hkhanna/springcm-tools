6964db78aa3d08fec4463f4e48ab3773ef698ae6
Harry Khanna <harry@khanna.cc>
2019-06-01 16:24:25 -0400

    Deployment debugging

--- config/wsgi.py
+++ config/wsgi.py
@@ -11,6 +11,7 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings.production')
+os.environ.setdefault('DJANGO_READ_DOT_ENV_FILE', 'True')
 
 application = get_wsgi_application()
