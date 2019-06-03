e67d64aa70e7abd65b283370aaa01976720cadc8
Harry Khanna <harry@khanna.cc>
2019-06-03 09:10:40 -0400

    Install django-debug-toolbar

--- config/settings/local.py
+++ config/settings/local.py
@@ -4,3 +4,7 @@ from .base import env
 DEBUG = True
 SECRET_KEY = env('DJANGO_SECRET_KEY', default="!LOCAL SECRET KEY!")
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0"]
+
+INSTALLED_APPS = ['debug_toolbar'] + INSTALLED_APPS
+MIDDLEWARE = ['debug_toolbar.middleware.DebugToolbarMiddleware'] + MIDDLEWARE
+INTERNAL_IPS = ['127.0.0.1']
