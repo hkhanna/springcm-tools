4ca24157d516c0efe2bba31eec637f32446601e0
Harry Khanna <harry@khanna.cc>
2019-07-06 21:56:35 -0400

    Make website look decent
    Closes #3

--- springcm_tools/linter/forms.py
+++ springcm_tools/linter/forms.py
@@ -1,4 +1,16 @@
 from django import forms
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import HTML, Layout, Div, Field
 
 class UploadFileForm(forms.Form):
-    file = forms.FileField(label="Select a SpringCM Template", help_text="max. 2MB")
+    file = forms.FileField(label="Upload SpringCM Template (.docx)")
+
+    def __init__(self, *args, **kwargs):
+        super(UploadFileForm, self).__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.form_method = 'POST'
+        self.helper.form_action = 'linter:index'
+        self.helper.layout = Layout(
+            Div('file'),
+            HTML('<hr /><button type="submit" class="btn btn-primary"><span class="fas fa-compress"></span> Check for Errors</button>')
+        )
