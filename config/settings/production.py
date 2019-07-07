a0a976bbec556344783ba8c0a976e5135068fff9
Harry Khanna <harry@khanna.cc>
2019-07-07 12:03:31 -0400

    Fix static files directory

--- config/settings/production.py
+++ config/settings/production.py
@@ -4,5 +4,5 @@ from .base import env
 DEBUG = False
 SECRET_KEY = env('DJANGO_SECRET_KEY')
 ALLOWED_HOSTS = ["localhost", "127.0.0.1", "0.0.0.0", "springcm.khanna.cc"]
-STATIC_ROOT = str(ROOT_DIR.path("staticfiles"))
+STATIC_ROOT = str(ROOT_DIR.path("static"))
 UPLOAD_DIR = str(ROOT_DIR.path("uploads"))
