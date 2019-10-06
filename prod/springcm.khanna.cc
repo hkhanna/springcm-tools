323646e5496ef9423114e2dfa98e31231de239ce
Harry Khanna <harry@khanna.cc>
2019-10-06 14:51:21 -0400

    Update ansible playbook to deploy to sky

--- prod/springcm.khanna.cc
+++ prod/springcm.khanna.cc
@@ -1,1 +1,1 @@
-web ansible_host=68.183.101.197 ansible_port=22 ansible_user=harry ansible_ssh_common_args='-o ControlMaster=auto -o ControlPersist=60s -o ForwardAgent=yes -o StrictHostKeyChecking=no' enable_default_server=False
+web ansible_host=sky.khanna.cc ansible_port=22 ansible_user=harry ansible_ssh_common_args='-o ControlMaster=auto -o ControlPersist=60s -o ForwardAgent=yes -o StrictHostKeyChecking=no' enable_default_server=False
